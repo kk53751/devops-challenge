67644a6fb8022245077bf9d398bf3ea521b2e16d
kk53751 <kk53751@gmail.com>
2025-01-24 23:25:43 +0530

    Update s3 and provider

--- provider.tf
+++ provider.tf
@@ -9,30 +9,19 @@ terraform {
   required_version = ">= 1.9.0"
 
   backend "s3" {
-    bucket         = "terraform-state-bucket" # Replace with your desired bucket name
+    bucket         = "lon-terraform-bucket" # Replace with your desired bucket name
     key            = "terraform/state"
-    region         = "us-west-2"
+    region         = "eu-west-2"
     dynamodb_table = "terraform-locks"
     encrypt        = true
   }
 }
 
 provider "aws" {
-    region = "us-west-2" # Change to your desired region
+    region = "eu-west-2" # Change to your desired region
     access_key = var.AWS_ACCESS_KEY_ID	
     secret_key = var.AWS_SECRET_ACCESS_KEY	
 
  }
 
-resource "aws_s3_bucket" "my_bucket" {
-  bucket = "terraform-state-bucket" # Replace with your desired bucket name
 
-  tags = {
-    Name        = "MyTerraformBackendBucket"
-    Environment = "Development"
-  }
-
-  lifecycle {
-    prevent_destroy = true # Prevents the bucket from being destroyed
-  }
-}
